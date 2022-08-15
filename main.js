8a673590243ddc25f1cf7af6b7b67106786e3188
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-15 13:22:48 -0300

    Preenchendo o formulario usando o dados recebidos da API

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 async function buscaEndereco(cep) {
+   let mensagemErro = document.querySelector("#erro");
+   mensagemErro.innerHTML = '';
     try {
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('Cep não existente');
         }
+
+        let cidade = document.querySelector('#cidade');
+        let logradouro = document.querySelector('#endereco');
+        let estado = document.querySelector('#estado');
+        let bairro = document.querySelector('#bairro');
+        
+        bairro.value = consultaCEPConvertida.bairro;
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = '<p>CEP inválido. Tente novamente!</p>'
         console.log(erro);
     }
 }
